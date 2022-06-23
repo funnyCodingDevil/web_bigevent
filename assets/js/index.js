23e4640624a36e735ed37d5cd997a21f76b39329
funnyCodingDevil <sjh201411@gmail.com>
2022-06-23 10:13:50 +0800

    完成article分支

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,10 +27,10 @@ function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
-        // headers 就是请求头配置对象
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers 就是请求头配置对象 放在baseAPI中
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: function (res) {
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
